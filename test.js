4881c09e9411047c6e59f012db3a816c8b2cf0fa
FlyingHavok <156331661+FlyingHavok@users.noreply.github.com>
2024-01-14 09:24:08 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initially show the "Please" cat GIF
-    showCatEmbed('2578585799545816842');
+    showCatImage('please.gif');
 });
 
 function handleYes() {
-    document.getElementById('question').innerText = 'Yay, finally! <3';
+    document.getElementById('question').innerText = 'Yay, finally!';
     // Show the dancing cat GIF after clicking Yes
-    showCatEmbed('25266051', 'dance 2s infinite');
+    showCatImage('dance.gif', 'dance 2s infinite');
     document.getElementById('yesBtn').style.display = 'none';
     document.getElementById('noBtn').style.display = 'none';
 }
@@ -20,9 +20,9 @@ function handleNoHover() {
     noBtn.style.top = randomY + 'px';
 }
 
-function showCatEmbed(postId, animation = '') {
+function showCatImage(src, animation = '') {
     const catDiv = document.getElementById('cat');
-    catDiv.innerHTML = `<iframe src="https://tenor.com/embed/${postId}/?autoplay=true" width="100%" height="100%" frameborder="0" class="tenor-gif-embed" allowfullscreen></iframe>`;
+    catDiv.style.backgroundImage = `url(${src})`;
     if (animation !== '') {
         catDiv.style.animation = animation;
     }
