db59364e283ade34248b8d1c5cb8feee9a64d152
FlyingHavok <156331661+FlyingHavok@users.noreply.github.com>
2024-02-08 09:48:09 +0530

    13

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ document.getElementById('cat').style.backgroundImage = "url('please.gif')";
 document.getElementById('musicBefore').src = "music_before.mp3";
 document.getElementById('musicAfter').src = "music_after.mp3";
 
+// Array of photo URLs
+const photos = [
+    'Photo 1.jpg',
+    'Photo 2.jpg',
+    'Photo 3.jpg',
+    'Photo 4.jpg',
+    'Photo 5.jpg',
+    'Photo 6.jpg',
+    'Photo 7.jpg',
+    'Photo 8.jpg'
+];
+
+let currentPhotoIndex = 0;
+
+// Function to update the photo in the fading frame
+function updatePhoto() {
+    const photoFrame = document.getElementById('photoFrame');
+    photoFrame.style.backgroundImage = `url('${photos[currentPhotoIndex]}')`;
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+}
+
+// Interval to update the photo every 5 seconds
+setInterval(updatePhoto, 5000);
+
 function handleYes() {
     document.getElementById('question').innerText = "Yay, you're my Valentine! 💖😍";
     document.getElementById('cat').style.backgroundImage = "url('dance.gif')";
@@ -14,61 +38,11 @@ function handleNoHover() {
     var noButton = document.getElementById('noBtn');
     var maxX = window.innerWidth - noButton.clientWidth;
     var maxY = window.innerHeight - noButton.clientHeight;
-    
+
     var randomX = Math.floor(Math.random() * maxX);
     var randomY = Math.floor(Math.random() * maxY);
-    
+
     noButton.style.position = 'absolute';
     noButton.style.left = randomX + 'px';
     noButton.style.top = randomY + 'px';
 }
-
-function playBeforeMusic() {
-    var musicBefore = document.getElementById('musicBefore');
-    if (musicBefore.paused) {
-        musicBefore.play();
-    } else {
-        musicBefore.pause();
-    }
-}
-
-function changeVolume() {
-    var volumeControl = document.getElementById('volumeControl');
-    var musicBefore = document.getElementById('musicBefore');
-    var musicAfter = document.getElementById('musicAfter');
-    
-    musicBefore.volume = volumeControl.value;
-    musicAfter.volume = volumeControl.value;
-}
-<<<<<<< HEAD
-=======
-
-// Function to add fading photos to the container
-function addFadingPhotos() {
-    const photoContainer = document.getElementById('photos-container');
-
-    for (let i = 1; i <= 8; i++) {
-        const photo = document.createElement('img');
-        photo.src = `Photo ${i}.jpg`; // Replace 'jpg' with the actual image format
-        photo.alt = `Photo ${i}`;
-        photo.className = 'photo';
-        setRandomPosition(photo);
-        photoContainer.appendChild(photo);
-    }
-}
-
-// Function to set a random position for the photo
-function setRandomPosition(photo) {
-    const maxX = window.innerWidth - 300;
-    const maxY = window.innerHeight - 300;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    photo.style.left = `${randomX}px`;
-    photo.style.top = `${randomY}px`;
-}
-
-// Call the function to add fading photos
-addFadingPhotos();
->>>>>>> 57bbda81f76461d2e22520db8c74bf6d0cf6571d
